6be0624a0a58c2e5876e1b92ab541c88fa86de73
Pedro Alves <alvespedro.dev@gmail.com>
2020-01-25 02:49:09 -0300

    Primeiro commit

---
+++ Assets/Scripts/movingsprite.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movingsprite : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(new Vector3(0,0.3f,0));
+        transform.Rotate(new Vector3(0, 0, 4.0f));
+        //transform.localScale += new Vector3(-0.1f,-0.1f,0);
+
+    }
+}
